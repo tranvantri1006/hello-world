ac70c53306c959386b5fc33bc81b4c94676487e3
tranvantri1006 <tranvantri1006@gmail.com>
2022-05-18 13:53:52 +0700

    1st lam chuyen ay-upload code to githhub

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author Admin
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {  
+        System.out.println("Welcome to GitHub &Java World");
+        // TODO code application logic here
+    }
+    
+}
